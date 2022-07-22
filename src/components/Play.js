17a62ee70efe9e6569f558292347d28a52d74b5d
time2fishman <adam_h_martinez@hotmail.com>
2022-07-21 21:42:46 -0600

    styling Play component and working on radio buttons

--- src/components/Play.js
+++ src/components/Play.js
@@ -5,10 +5,18 @@ import { Navigate } from 'react-router-dom'
 function Play({ difficulty, category }) {
     const [results, setResults] = useState([])
     const [userAnswer, setUserAnswer] = useState()
+    const [checked, setChecked]= useState(true)
+
+    // function resetRadioState() {
+    //     setRadioState()
+    // }
 
     function handleAnswer(event) {
         setUserAnswer(event.target.value)
+        // setRadioState(event.target.value)
+        // resetRadioState()
     }
+
 
     function getResults() {
         const url = `https://opentdb.com/api.php?amount=1&difficulty=${difficulty}&category=${category}`
@@ -35,29 +43,30 @@ function Play({ difficulty, category }) {
     }
 
     return (
-        <main className='question-box'>
+        <main>
             {results.map((result, i) => {
                 return (
-                    <div key={`correctKey:${i}`}>
+                    <div className='question-box' key={`correctKey:${i}`}>
                         <p className='question' dangerouslySetInnerHTML={{ __html: result.question }}></p>
                         <div className='possible-answers'>
                             <div className='choice'>
-                                <input className='answer' onChange={handleAnswer} type='radio' id='correctAnswer' name='correctAnswer' value={result.correct_answer} />
-                                <label className='answers' htmlFor='correctAnswer' dangerouslySetInnerHTML={{ __html: result.correct_answer }}></label>
+                                <input className='answer-button' onChange={handleAnswer} type='radio' id='correctAnswer' name='answer' value={result.correct_answer} />
+                                <label className='answer-label' htmlFor='correctAnswer' dangerouslySetInnerHTML={{ __html: result.correct_answer }}></label>
                             </div>
 
                             {result.incorrect_answers.map((incorrectAnswer, i) => (
                                 <div key={`incorrectKey:${i}`} className='choice'>
-                                    <input className='answers' onChange={handleAnswer} type='radio' id='incorrectAnswer' name='incorrectAnswer' value={incorrectAnswer} />
-                                    <label className='answers' htmlFor='incorrectAnswer' dangerouslySetInnerHTML={{ __html: incorrectAnswer }}></label>
+                                    <input className='answer-button'
+                                     onChange={handleAnswer} type='radio' id='incorrectAnswer' name='answer' value={incorrectAnswer} />
+                                    <label className='answer-label' htmlFor='incorrectAnswer' dangerouslySetInnerHTML={{ __html: incorrectAnswer }}></label>
                                 </div>
                             ))}
-                            {(userAnswer === results[0].correct_answer) && <div className='next-question'>
-                                <h2>Correct!</h2>
+                            {(userAnswer === results[0].correct_answer) && <div>
+                                <h2>*** Correct! ***</h2>
                                 <button className='next-question' type='button' onClick={getResults}>Next Question</button>
                             </div>}
-                            {(userAnswer === results[0].incorrect_answers[0] || userAnswer === results[0].incorrect_answers[1] || userAnswer === results[0].incorrect_answers[2]) && <div className='next-question'>
-                                <h2>Incorrect, better luck next time.</h2>
+                            {(userAnswer === results[0].incorrect_answers[0] || userAnswer === results[0].incorrect_answers[1] || userAnswer === results[0].incorrect_answers[2]) && <div>
+                                <h2>*** Incorrect! ***</h2>
                                 <button className='next-question' type='button' onClick={getResults}>Next Question</button>
                             </div>}
                         </div>
